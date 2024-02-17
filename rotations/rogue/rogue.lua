a87dc0b6f713b916cba38987a156b4ca65c802a5
ferib <lopke123@live.nl>
2024-02-17 18:05:38 +0100

    updated rogue for PvP

--- rotations/rogue/rogue.lua
+++ rotations/rogue/rogue.lua
@@ -2,16 +2,29 @@ local addon, light = ...
 
 local inCombat = false
 
+local inOpener = false
+
 local function opener()
 	if player.alive and target.exists and target.alive and target.enemy then
 		-- either throw or attack
 		-- TODO: castable check?
+		--[[
 		if target.in_range("Throw") then
 			cast("Throw")
 			return cast("Attack")
 		--elseif target.in_range("Attack") then
-		elseif target.in_range("Sinister Strike") then -- melee range check
-			return cast("Attack")
+		else ]]--
+		
+		--if target.in_range("Sinister Strike") then -- melee range check
+		--	return cast("Attack")
+		--end
+
+		--if target.in_range("Sinister Strike", target) and castable(SB.Ambush, target) then
+		--	return cast(SB.Ambush, target)
+		--end
+		if target.in_range("Sinister Strike", target) and castable(SB.CheapShot, target) then
+			inOpener = true
+			return cast(SB.CheapShot, target)
 		end
 	end
 end
@@ -47,9 +60,9 @@ local function combat()
 
 
 	-- check if not in range
-	if not target.in_range("Sinister Strike") and target.in_range("Throw") and castable("Throw") then
-		cast("Throw")
-	end
+	--if not target.in_range("Sinister Strike") and target.in_range("Throw") and castable("Throw") then
+	--	cast("Throw")
+	--end
 
 	-- add Gouge to get 1 combo point and recove energy or interrupt in spell casting?
 
@@ -63,13 +76,28 @@ local function combat()
 	--	return cast(SB.ExposeArmor, target)
 	--end
 
+	-- KidneyShot if in opener?
+	print(inOpener)
+	if inOpener and -player.power.combopoints >= 3 and -player.power.energy >= 25 and castable(SB.KidneyShot, target) then
+		inOpener = false
+		return cast(SB.KidneyShot, target)
+	end
+
 	-- Eviscerate on 4+ while SliceAndDice already on
-	if -player.power.combopoints >= 4 and -player.power.energy > 40 and castable(SB.Eviscerate, target) and target.in_range("Eviscerate") then
+	-- OR 2+ combo points if target low health?
+	if (-player.power.combopoints >= 4 or (target.health.actual < 300 and -player.power.combopoints >= 2))
+		and -player.power.energy > 40 and castable(SB.Eviscerate, target) and target.in_range("Eviscerate") then
+		inOpener = false
+		if toggle("cooldowns", false) then
+			cast(SB.ColdBlood)
+		end
 		return cast(SB.Eviscerate, target)
 	end
 	
 	-- 424785
 	--print(target.debuff(SB.SaberSlash).up )
+
+	-- NOTE: only keep this up for PvE long fights?
 	if -player.power.energy > 40 and castable(SB.SinisterStrike, target)
 		and (not target.debuff(SB.SaberSlash).up 
 			or target.debuff(SB.SaberSlash).count < 3
